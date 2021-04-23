95a9b5d4156de0101518559c77abd759b3481e5a
Silvia Taliana <silviataliana2@gmail.com>
2021-04-23 17:26:45 +0930

    adding table

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Search from "./components/Search/Searchbox";
 import Header from "./components/Header/Header";
+import Table from "./components/Table/Table";
 
 function App() {
   return (
     <div>
       <Header />
       <Search />
+      <Table />
     </div>
   );
 }
